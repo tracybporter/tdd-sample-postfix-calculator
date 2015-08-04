fc2caa57e241fdd3ecf83ac7581bae69f792d83f
Tracy B Porter <tbporter@gmail.com>
2015-08-04 08:51:29 -0500

    tracy - button labels and values align.

--- tdd-sample-postfix-calculatorAcceptanceTests/MultiplicationFnTest.m
+++ tdd-sample-postfix-calculatorAcceptanceTests/MultiplicationFnTest.m
@@ -14,10 +14,20 @@
     [super tearDown];
 }
 
-- (void)testDisplayShowsNumberWhenButtonsSelected {
+- (void)testAllNumberButtonsWork {
     [tester tapViewWithAccessibilityLabel:@"One"];
-    UILabel *displayLabel = (UILabel *)[tester waitForViewWithAccessibilityLabel:@"ResultsDisplay"];
-    XCTAssertEqualObjects(@"1", [displayLabel text]);
+    [tester tapViewWithAccessibilityLabel:@"Three"];
+    [tester tapViewWithAccessibilityLabel:@"Five"];
+    [tester tapViewWithAccessibilityLabel:@"Seven"];
+    [tester tapViewWithAccessibilityLabel:@"Nine"];
+    [tester tapViewWithAccessibilityLabel:@"Zero"];
+    [tester tapViewWithAccessibilityLabel:@"Two"];
+    [tester tapViewWithAccessibilityLabel:@"Four"];
+    [tester tapViewWithAccessibilityLabel:@"Six"];
+    [tester tapViewWithAccessibilityLabel:@"Eight"];
+    [tester tapViewWithAccessibilityLabel:@"One"];
+    UILabel *resultsDisplay = (UILabel *)[tester waitForViewWithAccessibilityLabel:@"ResultsDisplay"];
+    XCTAssertEqualObjects(@"13579024681", [resultsDisplay text]);
 }
 
 @end
