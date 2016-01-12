d00b505d87c09f51dcf73f6ace0180a00660488f
Tracy B Porter <tbporter@gmail.com>
2016-01-12 10:10:39 -0600

    tracy - acceptance test RED.

---
+++ tdd-sample-postfix-calculatorAcceptanceTests/DivisionFnTest.m
@@ -0,0 +1,33 @@
+#import "PHGBaseFnTest.h"
+
+@interface DivisionFnTest : PHGBaseFnTest
+
+@end
+
+@implementation DivisionFnTest
+
+- (void)test61_dividedBy_Four {
+    [tester tapViewWithAccessibilityLabel:@"Six"];
+    [tester tapViewWithAccessibilityLabel:@"One"];
+
+    [tester tapViewWithAccessibilityLabel:@"Enter"];
+
+    [tester tapViewWithAccessibilityLabel:@"Four"];
+
+    [tester tapViewWithAccessibilityLabel:@"Enter"];
+
+    [tester tapViewWithAccessibilityLabel:@"Divide"];
+
+    XCTAssertEqualObjects(@"15.250000", [self retrieveDisplayValue]);
+}
+
+- (void)testSingleNumberHandledCorrectly {
+    [tester tapViewWithAccessibilityLabel:@"Three"];
+
+    [tester tapViewWithAccessibilityLabel:@"Enter"];
+    [tester tapViewWithAccessibilityLabel:@"Divide"];
+
+    XCTAssertEqualObjects(@"3", [self retrieveDisplayValue]);
+}
+
+@end
